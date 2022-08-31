a0f8cfafdd024c353fc679ec0a61bc82cdb7730f
“Matheus” <matheus.icara.vq@gmail.com>
2022-08-30 21:06:28 -0300

    atualizacoes

--- todo.js
+++ todo.js
@@ -1,10 +1,36 @@
-document.getElementById('adicionar').addEventListener('click', function() {
-    const inputTarefa = document.getElementById('tarefa');
+//document.getElementById('adicionar').onclick = function() {...}
+const inputTarefa = document.getElementById('tarefa');
+// inputTarefa.onkeyup
+inputTarefa.onkeydown = function (event) {
+    if (event.key === 'Enter') { adicionarTarefa();}
+ }
+document.getElementById('adicionar').addEventListener('click', adicionarTarefa);
+function adicionarTarefa(){
     const tarefa =inputTarefa.value;
     if(tarefa.trim()) {
         const li = document.createElement('li');
-        li.innerText = tarefa;
-        document.getElementById('tarefas').appendChild(li);
-        
+        const span = document.createElement('span');
+        const textNode = document.createTextNode(tarefa.trim());
+        //li.innerText = tarefa;
+        //li.innerHTML = tarefa;
+        span.appendChild(textNode);
+        span.onclick = function () {
+            if(span.classList.contains('done')){
+               span.classList.remove('done');
+            }else {
+                span.classList.add('done');
+            }
+        };    
+        li.appendChild(span);
+        const ol = document.getElementById('tarefas');
+        const button = document.createElement('button');
+        button.appendChild(document.createTextNode('Excluir'));
+        button.onclick = function () {
+            if (confirm('Deseja excluir?')) {  ol.removeChild(li); }
+        };
+        li.appendChild(button);
+        ol.appendChild(li);
+        inputTarefa.value = '';
+        alert('Tarefa adicionada com sucesso!');
     }
-});
+};
